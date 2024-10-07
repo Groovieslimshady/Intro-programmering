9eb5ee6deb170a421327fc1708c2d5dd8996cba8
Groovieslimshady <tom.boden06@skola.stenungsund.se>
2024-10-07 10:11:16 +0200

    klar

---
+++ 7060 slingor - while/uppgift_2.py
@@ -0,0 +1,12 @@
+text = input ("Hur många heltal vill du ha? ")
+text2 = input ("Vilket ska det minsta talet vara? ")
+text2 = int (text2)
+text = int (text)
+i = text2
+nytt = text
+
+while i < text2 + nytt:
+    print (i)
+    i = i + 1
+
+print ("klart")
