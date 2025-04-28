fad4177c5183cd73856b45fc28fdfd387e7c82a5
Groovieslimshady <tom.boden06@skola.stenungsund.se>
2025-04-28 10:11:19 +0200

    update

--- Tkinter/increase-decrease.py
+++ Tkinter/increase-decrease.py
@@ -14,12 +14,12 @@ window.rowconfigure(0, minsize=50, weight=1)
 window.columnconfigure([0, 1, 2], minsize=50, weight=1)
 
 btn_decrease = tk.Button(master=window, text="-", command=decrease)
-btn_decrease.grid(row=0, column=0, sticky="nsew")
+btn_decrease.grid(row=0, column=0, sticky="ne")
 
 lbl_value = tk.Label(master=window, text="0")
-lbl_value.grid(row=0, column=1)
+lbl_value.grid(row=0, column=1, sticky="n")
 
 btn_increase = tk.Button(master=window, text="+", command=increase)
-btn_increase.grid(row=0, column=2, sticky="nsew")
+btn_increase.grid(row=0, column=2, sticky="nw")
 
 window.mainloop()
--- Tkinter/tk-widget-theme.py
+++ Tkinter/tk-widget-theme.py
@@ -5,5 +5,6 @@ root = tk.Tk()
 
 tk.Label (root, text='classic label').pack()
 ttk.Label (root, text="themed label").pack()
+
 root.geometry ("1000x600+50+50")
 root.mainloop()
